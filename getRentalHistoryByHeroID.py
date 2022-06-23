2d908fe8ae5514b012a7dc11dcdc59e6b3c23211
longzai <xxlongzaixx@outlook.com>
2022-06-23 18:40:05 +0800

    Create getRentalHistoryByHeroID.py

---
+++ getRentalHistoryByHeroID.py
@@ -0,0 +1,62 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+from collections import OrderedDict
+from operator import getitem
+import time, datetime
+
+########## Configuration String ##########
+
+GRAPHQL_URL = "https://defi-kingdoms-community-api-gateway-co06z8vi.uc.gateway.dev/graphql"
+HERO_ID = "700"
+
+##########################################
+
+transport = AIOHTTPTransport(url=GRAPHQL_URL)
+
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+graphQuery = """
+{
+  assistingAuctions(where: {tokenId: "HERO_ID"}, orderBy: id, orderDirection: desc) {
+    id
+    seller {
+      id
+    }
+    tokenId {
+      id
+    }
+    startingPrice
+    endingPrice
+    duration
+    startedAt
+    winner {
+      id
+    }
+    endedAt
+    open
+    purchasePrice
+  }
+}
+
+"""
+
+graphQuery = graphQuery.replace("HERO_ID",HERO_ID)
+
+query = gql(
+    graphQuery
+)
+
+# Execute the query on the transport
+result = client.execute(query)
+print(f"Rental Data for Heroes #{HERO_ID}  ===")
+for item in result['assistingAuctions']:
+    status = ""
+    if item['purchasePrice'] is not None:
+        #print(item['purchasePrice'][:-18])
+        status = u'\u2713'
+    else:
+        status = ""
+    duration = item['endedAt'] - item['startedAt']
+
+    d = datetime.datetime.fromtimestamp(int(item['endedAt']))
+    print(d.strftime("%d-%b-%y  %H: %M"), " Price:" , item['startingPrice'][:-18], status, item['seller']['id'][-4:] )
